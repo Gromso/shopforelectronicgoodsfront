65fdc96a31534b151646532391d35c4ee94d0631
Sinisa Gromovic <gromso97@gmail.com>
2023-09-05 17:48:47 +0200

    Create the Main menu component - static

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-
+import { Container} from 'react-bootstrap';
+import {faHome} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { MainMenu } from '../MainMenu/MainMenu';
 function App() {
   return (
-    <div className="App">
-     App component
-    </div>
+    <Container>
+     <FontAwesomeIcon icon={faHome} /> Home 
+    </Container>
   );
 }
 
